640106136a512fe2d3d49ec294c2b42a1c1ae6ed
Keisuke Izumiya <unstitch_serpent852@simplelogin.com>
2024-10-09 05:25:57 +0900

    [other] test

--- tests/foo/test.nim
+++ tests/foo/test.nim
@@ -9,6 +9,10 @@ check 1 + 2 == 3
 
 import std/os
 static:
-  echo staticRead(currentSourcePath().parentDir.parentDir.parentDir / "github_actions_sandbox.nimble")
+  echo when defined(js): "===JS===" else "===C==="
+  let s = currentSourcePath().parentDir.parentDir.parentDir / "github_actions_sandbox.nimble"
+  echo "===DIR: ", s
+  echo staticRead(s)
+  echo "===FIN==="
 when defined(windows):
   check false
